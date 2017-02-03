28f2bcb7dd7b5be7af2f46e23ac955ed9dfef002
solene <solenantoine@gmail.com>
2017-02-03 14:34:10 +0100

    pfiouuu début js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-console.log("Youpi");
+var contenu = document.getElementById("hidden");
+var titre = document.getElementById ("cache");
+titre.addEventListener("click", function() {
+    contenu.style.display = "none";
+});
+// titre.addEventListener("click", function() {
+// contenu.style.display = "block";
+// });

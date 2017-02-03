e019ac7d1e43cc5a44a592a70592ebd044e026ba
solene <solenantoine@gmail.com>
2017-02-03 15:48:53 +0100

    ...

--- script/script.js
+++ script/script.js
@@ -1,9 +1,18 @@
-var contenus = document.getElementsByClassName("hidden");
 var titres = document.getElementsByClassName("cache");
+var contenus = document.getElementsByClassName("hidden");
+function cdukk(element) {
+    for (var i = 0; i < contenus.length; i++) {
+            contenus[i].style.display = "none" ;
+    }
+    element.class cache tyle.display = "block";
+}
 
-titre.addEventListener("click", function() {
-    contenus.style.display = "none";
-});
+
+
+//
+// titre.addEventListener("click", function() {
+//     contenus.style.display = "none";
+// });
 // titre.addEventListener("click", function() {
 // contenu.style.display = "block";
 // });
